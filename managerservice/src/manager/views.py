e4f5d579049b1ab5658817834eee5ada7807a55c
opielapatryk <xpatrykopiela@gmail.com>
2024-06-03 14:55:30 +0200

    system view set

--- managerservice/src/manager/views.py
+++ managerservice/src/manager/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from .serializers import SystemSerializer
+from .models import System
 
-# Create your views here.
+
+class SystemViewSet(ModelViewSet):
+    """
+    A viewset for viewing and editing system instances.
+    """
+
+    serializer_class = SystemSerializer
+    queryset = System.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the systems
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        return System.objects.filter(owner_id=user.id)
