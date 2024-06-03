a07753ba6bdf2d135b5838a31d0abb6496f6e552
opielapatryk <xpatrykopiela@gmail.com>
2024-06-03 14:57:53 +0200

    viewset test

--- managerservice/src/manager/tests.py
+++ managerservice/src/manager/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from .models import System
 
-# Create your tests here.
+
+class SystemViewSetTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword"
+        )
+        self.client.login(username="testuser", password="testpassword")
+        self.system = System.objects.create(
+            name="Test System",
+            system_type="NFT",
+            capacity=100.0,
+            owner_id=self.user.id,
+        )
+        self.token = self.get_token_for_user(self.user)
+
+    def get_token_for_user(self, user):
+        refresh = RefreshToken.for_user(user)
+        return str(refresh.access_token)
+
+    def test_get_systems(self):
+        self.client.credentials(HTTP_AUTHORIZATION="Bearer " + self.token)
+        response = self.client.get("/api/v1/system/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["results"]), 1)
+        self.assertEqual(response.data["results"][0]["name"], "Test System")
