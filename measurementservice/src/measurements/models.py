f4153a60f62653ca64767e2a4dac5db13e2ec463
opielapatryk <xpatrykopiela@gmail.com>
2024-06-03 15:10:33 +0200

    measurements - model, serializer

--- measurementservice/src/measurements/models.py
+++ measurementservice/src/measurements/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Measurement(models.Model):
+    system_id = models.IntegerField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    ph = models.FloatField()
+    water_temperature = models.FloatField()
+    tds = models.FloatField(help_text="Total Dissolved Solids")
+
+    def __str__(self):
+        return f"{self.system_id, self.timestamp, self.ph, self.water_temperature, self.tds}"
---
+++ measurementservice/src/measurements/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Measurement
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = "__all__"
