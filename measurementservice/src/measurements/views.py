965fd36c13ef1c04d76f41f84b1395e4c32373b6
opielapatryk <xpatrykopiela@gmail.com>
2024-06-03 15:15:56 +0200

    producer in measurements viewset

--- measurementservice/src/measurements/views.py
+++ measurementservice/src/measurements/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializers import MeasurementSerializer
 from .models import Measurement
+from .producer import ProducerMeasurement
 
 
 class MeasurementViewSet(ModelViewSet):
@@ -10,3 +11,28 @@ class MeasurementViewSet(ModelViewSet):
 
     serializer_class = MeasurementSerializer
     queryset = Measurement.objects.all()
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the viewset and the RabbitMQ producer.
+        """
+        super().__init__(*args, **kwargs)
+        self.producer = ProducerMeasurement()
+
+    def perform_create(self, serializer):
+        """
+        Save a new measurement instance and send a message to RabbitMQ.
+
+        This method saves a new measurement instance using the provided serializer,
+        then sends a message containing the measurement details to RabbitMQ.
+        """
+        instance = serializer.save()
+        message = {
+            "id": instance.id,
+            "system_id": instance.system_id,
+            "timestamp": instance.timestamp.isoformat(),  # Convert datetime to ISO format string
+            "ph": instance.ph,
+            "water_temperature": instance.water_temperature,
+            "tds": instance.tds,
+        }
+        self.producer.publish(message)
