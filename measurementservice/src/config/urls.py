ffb6ab4f394206f2ae793fe2c82ce10fe3744572
opielapatryk <xpatrykopiela@gmail.com>
2024-06-03 15:09:31 +0200

    url setup

--- measurementservice/src/config/urls.py
+++ measurementservice/src/config/urls.py
@@ -15,9 +15,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.urls import include, path, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 from django.contrib import admin
-from django.urls import path
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Measurements API",
+        default_version="v0.1.0",
+        description="Public django API for measurements",
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    url="http://127.0.0.1:8000/api/v1/measurement/",
+)
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/v1/measurement/admin/", admin.site.urls),
+    path("", include("measurements.urls")),
+    re_path(
+        "api/v1/schema/measurement",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
