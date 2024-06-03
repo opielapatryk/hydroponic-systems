ea9937e415a0fbb6db2db4e1951210a1bdf4741d
opielapatryk <xpatrykopiela@gmail.com>
2024-06-03 14:22:09 +0200

    urls, test pass

--- authservice/src/config/urls.py
+++ authservice/src/config/urls.py
@@ -16,8 +16,29 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Authentication API",
+        default_version="v0.1.0",
+        description="Public django API for user authentication based on JWT",
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+    url="http://127.0.0.1:8000/api/v1/auth/",
+)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("api/v1/auth/admin/", admin.site.urls),
+    path("api/v1/auth/", include("authentication.urls")),
+    re_path(
+        "api/v1/schema/auth",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
 ]
